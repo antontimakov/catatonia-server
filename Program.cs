fe2761839d23a0c38625546a3c2dead3a17bae5a
timakov <atimakov@srv.rbt>
2025-12-18 21:28:30 +0300

    очистка

--- Program.cs
+++ Program.cs
@@ -1,57 +1,46 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.StaticFiles;
-using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-
-using CatatoniaServer.Models;
-
+//using System.ComponentModel.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Добавляем контроллеры
 builder.Services.AddControllers();
-// Считываем строку подключения из appsettings.json
+
+// Подключаем БД
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseNpgsql(connectionString));
+    options.UseNpgsql(connectionString));
 
-// Добавляем поддержку MIME-типов для файлов Unity
-var provider = new FileExtensionContentTypeProvider();
-provider.Mappings[".data"] = "application/octet-stream";
-provider.Mappings[".wasm"] = "application/wasm";
-provider.Mappings[".js"] = "application/javascript";
-
-// Добавляем сервис для отображения содержимого директорий (если нужно)
-builder.Services.AddDirectoryBrowser(); // Убедитесь, что это необходимо в продакшене
+// Добавляем Swagger (только для разработки)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 var app = builder.Build();
 
+// Включаем Swagger только в режиме разработки
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.MapControllers();
+// Настройка MIME-типов для Unity
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".data"] = "application/octet-stream";
+provider.Mappings[".wasm"] = "application/wasm";
+provider.Mappings[".js"] = "application/javascript";
 
-app.Urls.Add("http://0.0.0.0:5074");
-
-// Обслуживание статических файлов с расширенными MIME-типами
+// Обслуживание статики
+app.UseDefaultFiles(); // Поддержка default-файлов (index.html, default.html и т.д.)
+app.UseStaticFiles();  // Отдача статических файлов (CSS, JS, изображения)
 app.UseStaticFiles(new StaticFileOptions
 {
     ContentTypeProvider = provider
 });
 
-// Обслуживание статических файлов из папки wwwroot
-app.UseDefaultFiles(); // Поддержка default-файлов (index.html, default.html и т.д.)
-app.UseStaticFiles();  // Отдача статических файлов (CSS, JS, изображения)
-
-// Подключаем маршрутизацию для MVC
+// Маршрутизация
 app.MapControllers();
 
 // Маршрутизация SPA: все нераспознанные запросы направляются в index.html
